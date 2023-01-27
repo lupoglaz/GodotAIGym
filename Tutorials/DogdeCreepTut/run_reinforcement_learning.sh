9b42021b74a971fa489613b339f6f5e30e310fce
Dohyeong Kim <kimbring2@gmail.com>
2023-01-28 05:52:09 +0900

    Update run_reinforcement_learning.sh

--- Tutorials/DogdeCreepTut/run_reinforcement_learning.sh
+++ Tutorials/DogdeCreepTut/run_reinforcement_learning.sh
@@ -6,7 +6,7 @@ tmux new-session -d -t impala_dodgecreep
 
 tmux new-window -d -n learner
 #COMMAND_LEARNER='python3.7 learner.py --env_num '"${NUM_ACTORS}"' --gpu_use '"${GPU_USE}"' --pretrained_model '"${PRE_TRAINED_MODEL}"''
-COMMAND_LEARNER='python3.7 learner.py --env_num '"${NUM_ACTORS}"' --gpu_use '"${GPU_USE}"''
+COMMAND_LEARNER='python learner.py --env_num '"${NUM_ACTORS}"' --gpu_use '"${GPU_USE}"''
 echo $COMMAND_LEARNER
 
 tmux send-keys -t "learner" "$COMMAND_LEARNER" ENTER
@@ -15,7 +15,7 @@ sleep 1.0
 
 for ((id=0; id < $NUM_ACTORS; id++)); do
     tmux new-window -d -n "actor_${id}"
-    COMMAND='MINERL_HEADLESS=1 python3.7 actor.py --env_id  '"${id}"''
+    COMMAND='MINERL_HEADLESS=1 python actor.py --env_id  '"${id}"''
     tmux send-keys -t "actor_${id}" "$COMMAND" ENTER
 
     sleep 4.0
